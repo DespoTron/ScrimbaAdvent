987cf1f7edfc1b59c26c8de0d0e59ce1527ed6e6
Thai Do <thai.hl.do@gmail.com>
2020-12-03 15:14:26 -0800

    Solution for Scrimba Advent day 3

--- day3.js
+++ day3.js
@@ -11,8 +11,21 @@
 // slice()
 
 function chunkyMonkey(values, size) {
-  
+  let results = [];
+
+  for(let i = 0, d = 0, s = 0; i < values.length; i++, s++) {
+    if (i > 0 && i % size === 0) {
+      d++;
+      s = 0;
+    }
+
+    if (results[d] === undefined) results[d] = []; 
+
+    results[d][s] = values[i];
+  }
+  return results;
 }
 
 
 console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
+console.log(chunkyMonkey([0, 1, 2, 3, 4, 5], 4));
