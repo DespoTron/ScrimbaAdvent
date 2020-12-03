6049b56c2d66c3cbd0184c991a91348185e4fc46
Thai Do <thai.hl.do@gmail.com>
2020-12-03 11:45:15 -0800

    Day 3 Scrimba Advent working on solution

---
+++ day3.js
@@ -0,0 +1,18 @@
+// Project Name: Chunky Monkey
+
+// Write a functino that splits an array (first argument) into groups the length of size
+// (second argument) and returns them as a two-dimensional array.
+
+// Example
+  // chunkyMonkey(["a", "b", "c", "d"], 2) should return [["a","b"],["c","d"]]
+  // chunkyMonkey([0, 1, 2, 3, 4, 5], 4) should return [[0,1,2,3],[4,5]].
+
+// Hints
+// slice()
+
+function chunkyMonkey(values, size) {
+  
+}
+
+
+console.log(chunkyMonkey(["a", "b", "c", "d"], 2));
